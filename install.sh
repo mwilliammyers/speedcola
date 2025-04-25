6b7e26adfe3d1b51de7073a1cbd110b09eb967c8
William Myers <mwilliammyers@gmail.com>
2025-04-24 23:35:56 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,17 +18,34 @@ die() {
 	exit "${rc}"
 }
 
-download_to_file() {
-	curl -LO "$@" || wget "$@" || die "Downloading $@ failed"
-}
-
 install_package() {
-	if [ ! -f "piu" ]; then
-		download_to_file "https://raw.githubusercontent.com/keithieopia/piu/master/piu" >/dev/null && \
-		chmod u+x piu
-	fi
-
-	./piu install "$@"
+    if [ -x "$(command -v apt-get)" ]; then
+        sudo apt-get install -y "${@}"
+    elif [ -x "$(command -v brew)" ]; then
+        HOMEBREW_NO_ANALYTICS=1 HOMEBREW_NO_GITHUB_API=1 brew install "${@}"
+    elif [ -x "$(command -v pacman)" ]; then
+        sudo pacman -Syu "${@}"
+    elif [ -x "$(command -v dnf)" ]; then
+        sudo dnf -y "${@}"
+    elif [ -x "$(command -v yum)" ]; then
+        sudo yum -y "${@}"
+    elif [ -x "$(command -v zypper)" ]; then
+        sudo zypper install "${@}"
+    elif [ -x "$(command -v pkg)" ]; then
+        sudo pkg install "${@}"
+    elif [ -x "$(command -v pkg_add)" ]; then
+        pkg_add "${@}"
+    elif [ -x "$(command -v port)" ]; then
+        sudo port install "${@}"
+    elif [ -x "$(command -v emerge)" ]; then
+        emerge "${@}"
+    elif [ -x "$(command -v pkgin)" ]; then
+        pkgin -y install "${@}"
+    elif [ -x "$(command -v nix-env)" ]; then
+        nix-env -i "${@}"
+    else
+        return 1
+    fi
 }
 
 git_pull_or_clone() {
